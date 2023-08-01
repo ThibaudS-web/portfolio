89878a903c73b930a99698bf3ab17c5baada624f
ThibaudS <thibaud.devpro@gmail.com>
2023-08-01 17:51:40 +0200

    Added CustomSelect Component

---
+++ src/components/select_project_type/CustomSelect.tsx
@@ -0,0 +1,96 @@
+import { motion, Variants } from "framer-motion"
+import { useRef, useState } from "react"
+import { Options, SelectOption, SelectContainer, Option } from "./selectStyle"
+import Arrow from "../svg/ArrowSelect"
+
+function CustomSelect() {
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const [selectedOption, setSelectedOption] = useState("Trier les projets")
+
+    const handleOpenSelect = () => {
+        setIsOpen(!isOpen)
+        setSelectedOption("Trier les projets")
+    }
+
+    const handleSelectedOption = (option: string) => {
+        setSelectedOption(option)
+        setIsOpen(false)
+    }
+
+    const optionVariants: Variants = {
+        open: {
+            opacity: 1,
+            y: 0,
+            transition: { type: "spring", stiffness: 300, damping: 24 }
+        },
+        closed: { opacity: 0, y: 20, transition: { duration: 0.2 } }
+    };
+
+    const arrowVariants: Variants = {
+        closed: {
+            rotate: 180,
+            transition: {
+                type: "spring",
+                stiffness: 250,
+                damping: 15
+            }
+        },
+        open: {
+            rotate: 0,
+            transition: {
+                duration: 0.2,
+                type: "spring",
+                stiffness: 250,
+                damping: 15
+            }
+        }
+    }
+
+    return (
+        <>
+            <SelectContainer
+                initial={false}
+                animate={isOpen ? "open" : "closed"}
+            >
+                <SelectOption
+                    onClick={handleOpenSelect}
+                    whileTap={{ scale: 0.97 }}
+
+                >
+                    {selectedOption}
+                    <motion.div
+                        initial={false}
+                        animate={isOpen ? "open" : "closed"}
+                        variants={arrowVariants}
+                    >
+                        <Arrow />
+                    </motion.div>
+                </SelectOption>
+                {isOpen ?
+                    <Options>
+                        <Option
+                            onClick={() => handleSelectedOption("Projets de formation")}
+                            initial="closed"
+                            animate="open"
+                            variants={optionVariants}
+                        >
+                            Projets de formation
+                        </Option>
+                        <Option
+                            onClick={() => handleSelectedOption("Projets personnels")}
+                            initial="closed"
+                            animate="open"
+                            variants={optionVariants}
+                        >
+                            Projets personnels
+                        </Option>
+                    </Options>
+                    :
+                    null
+                }
+            </SelectContainer>
+        </>
+    )
+}
+
+export default CustomSelect
--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,49 +1,12 @@
-import { useState } from "react"
 import AnimatedTitle from "../../components/animated_title/AnimatedTitle"
 // import useProjectsData from "../../hooks/useProjectsData"
 import { BlocPage } from "../home/homeStyles"
-import { CustomSelect, Options, SelectOption, Option } from "./projectsStyle"
-import Arrow from "../../components/svg/ArrowSelect"
-import { Variants, motion } from "framer-motion"
+import CustomSelect from "../../components/select_project_type/CustomSelect"
 
 function Projects() {
-    const [isOpen, setIsOpen] = useState<boolean>(false)
+
     // const { data } = useProjectsData()
-
-    const handleOpenSelect = () => {
-        setIsOpen(!isOpen)
-    }
-
-    const optionVariants: Variants = {
-        open: {
-            opacity: 1,
-            y: 0,
-            transition: { type: "spring", stiffness: 300, damping: 24 }
-        },
-        closed: { opacity: 0, y: 20, transition: { duration: 0.2 } }
-    };
-
-    const arrowVariants: Variants = {
-        closed: {
-            rotate: 180,
-            transition: {
-                type: "spring",
-                stiffness: 250,
-                damping: 15
-            }
-        },
-        open: {
-            rotate: 0,
-            transition: {
-                duration: 0.2,
-                type: "spring",
-                stiffness: 250,
-                damping: 15
-            }
-        }
-    }
-
-    console.log(isOpen)
+    // TODO: Go to implement text option on props CustomSelect
 
     return (
         <>
@@ -58,45 +21,7 @@ function Projects() {
                     avatarPaths={["assets/png/avatar_open_eyes.png",
                         "assets/png/avatar_wink.png"]}
                 />
-                <CustomSelect
-                    initial={false}
-                    animate={isOpen ? "open" : "closed"}
-                >
-                    <SelectOption
-                        onClick={handleOpenSelect}
-                        whileTap={{ scale: 0.97 }}
-
-                    >
-                        Trier les projets
-                        <motion.div
-                            initial={false}
-                            animate={isOpen ? "open" : "closed"}
-                            variants={arrowVariants}
-                        >
-                            <Arrow />
-                        </motion.div>
-                    </SelectOption>
-                    {isOpen ?
-                        <Options>
-                            <Option
-                                initial="closed"
-                                animate="open"
-                                variants={optionVariants}
-                            >
-                                Projets de formation
-                            </Option>
-                            <Option
-                                initial="closed"
-                                animate="open"
-                                variants={optionVariants}
-                            >
-                                Projets personnels
-                            </Option>
-                        </Options>
-                        :
-                        null
-                    }
-                </CustomSelect>
+                <CustomSelect />
             </BlocPage>
         </>
     )
